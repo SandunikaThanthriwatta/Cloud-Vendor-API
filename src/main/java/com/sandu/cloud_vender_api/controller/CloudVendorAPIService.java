07a5f9b5ca30a665644c02b0ccfe4e0f6b849e1d
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-06-03 13:38:05 +0530

    Implemented POST and PUT REST APIs

--- src/main/java/com/sandu/cloud_vender_api/controller/CloudVendorAPIService.java
+++ src/main/java/com/sandu/cloud_vender_api/controller/CloudVendorAPIService.java
@@ -1,6 +1,9 @@
 package com.sandu.cloud_vender_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,28 @@ import com.sandu.cloud_vender_api.model.CloudVendor;
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 
+    CloudVendor cloudVendor;
+
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId){
 
-        return new CloudVendor("c1","name1","address1","xxxx");
+        return cloudVendor;
+        // return new CloudVendor("c1","name1","address1","xxxx");
 
     }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor created successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        
+        this.cloudVendor=cloudVendor;
+        return "Cloud Vendor updated successfully";
+    }
+
 }
