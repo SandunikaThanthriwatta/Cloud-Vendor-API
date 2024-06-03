fa04756e7ae48b7d7ca005d6e42eef63e04d45dc
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-06-03 13:55:45 +0530

    Implemented DELETE REST API

--- src/main/java/com/sandu/cloud_vender_api/controller/CloudVendorAPIService.java
+++ src/main/java/com/sandu/cloud_vender_api/controller/CloudVendorAPIService.java
@@ -1,5 +1,6 @@
 package com.sandu.cloud_vender_api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,6 +36,13 @@ public class CloudVendorAPIService {
         
         this.cloudVendor=cloudVendor;
         return "Cloud Vendor updated successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        
+        this.cloudVendor=null;
+        return "Cloud Vendor deleted successfully";
     }
 
 }
